a61428932fa6e9a20efcf7455ee013046b325eb1
hemerson1 <>
2022-03-23 21:03:53 +0000

    Removed pygame welcome

--- RLDevelopment/__init__.py
+++ RLDevelopment/__init__.py
@@ -6,6 +6,11 @@ Created on Mon Mar 21 21:33:10 2022
 @author: hemerson
 """
 
+# Hide PyGame Welcome
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
+# Import packages
 from RLDevelopment.RLDevelopment.collection import collect_sample
 from RLDevelopment.RLDevelopment.evaluation import test_policy
 from RLDevelopment.RLDevelopment.processing import clean_sample, get_batch
