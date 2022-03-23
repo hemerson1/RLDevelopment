bb0ffc9f4e9c53773e89e80f5c85e86abc56bf3b
hemerson1 <emersonharry8@gmail.com>
2022-03-23 17:14:57 +0000

    Updated with new functions

--- RLDevelopment/__init__.py
+++ RLDevelopment/__init__.py
@@ -6,3 +6,6 @@ Created on Mon Mar 21 21:33:10 2022
 @author: hemerson
 """
 
+from RLDevelopment.RLDevelopment.collection import collect_sample
+from RLDevelopment.RLDevelopment.evaluation import test_policy
+from RLDevelopment.RLDevelopment.processing import clean_sample, get_batch
