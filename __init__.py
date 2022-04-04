702c67dbb74466cdac9da1422613306596c5b2d9
hemerson1 <>
2022-04-04 21:40:11 +0100

    Restructured folder

--- __init__.py
+++ __init__.py
@@ -10,7 +10,3 @@ Created on Mon Mar 21 21:33:10 2022
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
-# Import packages
-from .collection import collect_sample
-from .evaluation import test_policy
-from .processing import clean_sample, get_batch
