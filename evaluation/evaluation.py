69c321ef3b3532f75e3896368072851f9d6ffbbc
hemerson1 <emersonharry8@gmail.com>
2022-04-06 12:27:48 +0100

    test commit

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -18,6 +18,8 @@ test episodes.
 """
 def test_policy(env, policy, episodes, **kwargs):
     
+    test = False
+    
     # run the policy for a set number of episodes
     episode_reward, episode_timestep = list(), list()
     for ep in range(episodes): 
