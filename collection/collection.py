401bae1b0d387710ddeeff1ecd2628256189490b
hemerson1 <emersonharry8@gmail.com>
2022-06-25 18:18:22 +0100

    Parallelised data collection with added early stopping

--- collection/collection.py
+++ collection/collection.py
@@ -15,57 +15,120 @@ from collections import deque
 import numpy as np
 import pickle as pkl
 import matplotlib.pyplot as plt
+import pathos.multiprocessing as mp
+import multiprocessing
+import functools, itertools
 
 from .processing import get_batch
+
+"""
+Run a single data collection episode.
+"""
+def run_episode(seed, env, policy, **kwargs):
+    
+    if _abort.is_set(): 
+        return
+    
+    # initialise the data array
+    sequence_data = []
+    
+    # reset the environmental parameters
+    env.seed(seed)
+    state = env.reset()
+    done, timestep = False, 0
+
+    # loop through the episode
+    while not done:
+
+        # take an action and step the environment
+        action = policy.get_action(state=state)
+        next_state, reward, done, _ = env.step(action)  
+
+        # add a termination penalty
+        if done and timestep != kwargs.get("max_timestep", -1):
+            reward += kwargs.get("termination_penalty", 0)
+
+        # terminate if reached max timesteps
+        if timestep == kwargs.get("max_timestep", -1):
+            done = True
+
+        # log the data
+        sample = {"state": state, "next_state": next_state, 
+                  "action": action, "reward": reward, "done": done}
+        sequence_data.append(sample)
+
+        # update the variables
+        state = next_state
+        policy.update()     
+        timestep += 1
+    
+    # update the shared count
+    with _counter.get_lock():
+        _counter.value += len(sequence_data)
+        print('Collection: {}/{}'.format(_counter.value, _sample_size))
+        
+        # Terminate training prematurely
+        if _counter.value >= _sample_size:
+            print('Stopping worker early.')
+            _abort.set()
+    
+    return sequence_data
+
+
+"""
+Define the global variables for threaded
+collection algorithm.
+"""
+def init_globals(counter, sample_size, abort):
+    global _counter
+    global _sample_size
+    global _abort
+    _sample_size = sample_size
+    _counter = counter
+    _abort = abort
+
 
 """
 When given a gym environment and compatible policy maker the function 
 collects a sample of date for a specified period and saves a replay.
 """
-
 def collect_sample(env, policy, sample_size, **kwargs):
     
-    # initialise the memory
+    # initialise the memory and the worker
     memory = deque(maxlen=sample_size)
     
-    # data in episodes until memory is full    
-    while len(memory) < sample_size:
-        
-        # reset the environmental parameters
-        state = env.reset()
-        done, timestep = False, 0
-        
-        # loop through the episode
-        while not done:
-            
-            # monitor the sample size
-            if len(memory) % (sample_size // 10) == 0 and len(memory) > 0: 
-                print('Samples collected: {}'.format(len(memory)))
-                if len(memory) == sample_size:
-                    break
-            
-            # take an action and step the environment
-            action = policy.get_action(state=state)
-            next_state, reward, done, _ = env.step(action)  
-            
-            # add a termination penalty
-            if done and timestep != kwargs.get("max_timestep", -1):
-                reward += kwargs.get("termination_penalty", 0)
-            
-            # terminate if reached max timesteps
-            if timestep == kwargs.get("max_timestep", -1):
-                done = True
-            
-            # log the data
-            sample = {"state": state, "next_state": next_state, 
-                      "action": action, "reward": reward, "done": done}
-            memory.append(sample)
-            
-            # update the variables
-            state = next_state
-            policy.update()     
-            timestep += 1
+    # define the input function
+    input_func = functools.partial(
+        run_episode, env=env, 
+        policy=policy, 
+        **kwargs
+    )
     
+    # define the counter and abort events
+    abort_event = multiprocessing.Event()
+    counter_obj = multiprocessing.Value('i', 0)
+    
+    # create an iterator that terminates with pool
+    def pool_args():
+        for i in range(sample_size):
+            if not abort_event.is_set():
+                yield i    
+    
+    # define the pool
+    pool = mp.Pool(
+        kwargs.get("num_workers", 4), 
+        initializer=init_globals, 
+        initargs=(counter_obj, sample_size, abort_event)
+    )  
+    
+    # run the multiprocessing
+    list_output = pool.map(input_func, pool_args())
+        
+    # filter to only include values
+    clean_list = [item for item in list_output if item]    
+    clean_list = list(itertools.chain.from_iterable(clean_list))    
+    memory.extend(clean_list)    
+        
     # get the file name and path
     filepath = kwargs.get("filepath", "./") 
     filename = kwargs.get("filename", "training_sample")
@@ -74,7 +137,7 @@ def collect_sample(env, policy, sample_size, **kwargs):
     with open(filepath + filename + '.pkl', 'wb') as file:
         pkl.dump(memory, file)
         
-    return memory    
+    return memory
 
 
 """
